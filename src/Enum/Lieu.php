d9cacf5c04d3203b5c64196cdebf3796a994b6d3
mehdi ayachi <mehdi.ayachi@esprit.tn>
2025-04-09 18:38:24 +0100

    crud table event et reservation

---
+++ src/Enum/Lieu.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enum;
+
+enum Lieu: string
+{
+    case TUNIS = 'Tunis';
+    case ARIANA = 'Ariana';
+    case BEJA = 'Beja';
+    case BENAROUS = 'BenArous';
+    case BIZERTE = 'Bizerte';
+    case GABES = 'Gabès';
+    case GAFSA = 'Gafsa';
+    case MONASTIR = 'Monastir';
+    case NABEUL = 'Nabeul';
+    case SFAX = 'Sfax';
+    case SIDIBOUZID = 'SidiBouzid';
+    case SILIANA = 'Siliana';
+    case SOUSSE = 'Sousse';
+    case TATAOUINE = 'Tataouine';
+    case TOZEUR = 'Tozeur';
+    case ZAGHOUAN = 'Zaghouan';
+}
